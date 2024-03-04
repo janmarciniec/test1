1c20f70a973ee5ad46d3da2f2f6dd552eabed9b1
Jan Marciniec <emjan10@wp.pl>
2024-03-04 13:57:14 +0100

    Zmiany

--- index.php
+++ index.php
@@ -2,27 +2,19 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8" />
-		<link rel="icon" type="image/svg+xml" href="/mapplic-admin/public/favicon.png" />
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no" />
+		<link rel="icon" type="image/svg+xml" href="./favicon.png" />
 		<title>mapplic</title>
-		<style>
-			body {
-				font-family: sans-serif;
-			}
-			#content {
-				margin: 16px auto;
-				max-width: 1000px;
-				padding: 0 16px;
-			}
-		</style>
-		<link rel="stylesheet" type="text/css" href="/dist/mapplic/mapplic.css">
+		<link rel="stylesheet" href="style.css">
 	</head>
 	<body>
-		<a href="index-admin.html">Admin panel</a>
-		<div id="content">
-			<mapplic-map id="map" data-json="data.json"/>
-		</div>
 
-		<script id="mapplic-script" type="module" src="/mapplic/dist/mapplic.js"></script>
+	<a href="index-admin.html">Admin panel</a>
+
+	<div class="container">
+		<mapplic-map id="my-map" data-json="data.json">
+                	<script type="text/javascript" id="mapplic-script" src="https://mapplic.com/mapplic.js"></script>
+        	</mapplic-map>
+	</div>
+
 	</body>
 </html>
